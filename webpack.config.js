974ad7b4b9902158d6c846e5f6aa32ef2f2c5b80
Forty Morgan <dimazhmudikov@gmail.com>
2018-08-25 20:11:34 +0300

    Chenge entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: './client/src',
+  entry: [`${__dirname}/client/src`],
   output: {
     path: `${__dirname}/static`
   },
