77d9b37974ea73299b924b4fd7fc355d5c5527ff
Forty Morgan <dimazhmudikov@gmail.com>
2018-08-25 20:46:27 +0300

    Move server creation to function

--- server/server.js
+++ server/server.js
@@ -3,35 +3,37 @@ import express from 'express';
 import socketio from 'socket.io';
 import RpslsGame from './rpslsGame';
 
-const app = express();
-
-app.use(express.static('static'));
-
-const server = http.createServer(app);
-
-const io = socketio(server);
-
-let waitingPlayer;
-
-io.on('connection', (socket) => {
-  socket.emit('player', waitingPlayer ? 'p2' : 'p1');
-
-  if (waitingPlayer) {
-    new RpslsGame(waitingPlayer, socket);
-    waitingPlayer = null;
-  } else {
-    socket.emit('message', ({ author: 's', message: 'Waiting for an opponent' }));
-    socket.on('disconnect', () => waitingPlayer = null);
-    waitingPlayer = socket;
-  }
-
-  socket.on('message', (message) => {
-    io.emit('message', message);
+export default () => {
+  const app = express();
+  
+  app.use(express.static('static'));
+  
+  const server = http.createServer(app);
+  
+  const io = socketio(server);
+  
+  let waitingPlayer;
+  
+  io.on('connection', (socket) => {
+    socket.emit('player', waitingPlayer ? 'p2' : 'p1');
+  
+    if (waitingPlayer) {
+      new RpslsGame(waitingPlayer, socket);
+      waitingPlayer = null;
+    } else {
+      socket.emit('message', ({ author: 's', message: 'Waiting for an opponent' }));
+      socket.on('disconnect', () => waitingPlayer = null);
+      waitingPlayer = socket;
+    }
+  
+    socket.on('message', (message) => {
+      io.emit('message', message);
+    });
   });
-});
-
-server.on('error', (err) => {
-  console.error('Server error:', err);
-});
-
-export default server;
+  
+  server.on('error', (err) => {
+    console.error('Server error:', err);
+  });
+  
+  return server;
+};
