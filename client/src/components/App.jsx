a3f03e1beb3656881c378440a50458517cfdbf21
Forty Morgan <dimazhmudikov@gmail.com>
2018-08-26 17:54:46 +0300

    Change app component to 'dumb'

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,15 +3,12 @@ import Header from './Header';
 import Game from './Game';
 import Chat from './Chat';
 
-export default class App extends Component {
-  render() {
-    const { session } = this.props;
-    return (
-      <Fragment>
-        <Header />
-        <Game />
-        <Chat />
-      </Fragment>
-    );
-  }
-};
+const App = () => (
+  <Fragment>
+    <Header />
+    <Game />
+    <Chat />
+  </Fragment>
+);
+
+export default App;
