d967d5341a806157ce991e2a51da572454253312
Forty Morgan <dimazhmudikov@gmail.com>
2018-08-28 21:04:35 +0300

    Add tests for the game class

---
+++ __tests__/__assets__/player.js
@@ -0,0 +1,58 @@
+export default class Player {
+  constructor() {
+    this.state = {
+      messages: [],
+      name: '',
+      result: '',
+      turn: '',
+      opponent: '',
+      online: false,
+    };
+    this.subscriptions = {};
+  }
+
+  sendSystemMessage(message) {
+    const { messages } = this.state;
+    this.state.messages = [...messages, { author: 's', message }];
+  }
+
+  sendMessage(message) {
+    const { messages } = this.state;
+    this.state.messages = [...messages, message];
+  }
+
+  startGame(name) {
+    this.state.name = name;
+    this.state.online = true;
+  }
+
+  finishGame(result) {
+    this.state.result = result;
+  }
+
+  resetGame() {
+    this.state.result = '';
+    this.state.turn = '';
+    this.state.opponent = '';
+  }
+
+  sendOpponentsTurn(turn) {
+    this.state.opponent = turn;
+  }
+
+  sendTurn(turn) {
+    this.state.turn = turn;
+  }
+
+  onOpponentLeft() {
+    this.state.online = false;
+  }
+
+  subscribeTo(event, callback) {
+    this.subscriptions = { ...this.subscriptions, [event]: callback };
+  }
+
+  emitEvent(event, arg) {
+    this.subscriptions[event](arg);
+  }
+}
---
+++ __tests__/game.js
@@ -0,0 +1,204 @@
+import RpslsGame from '../server/rpslsGame';
+import Player from './__assets__/player';
+
+describe('game instance test', () => {
+  const p1 = new Player();
+  const p2 = new Player();
+
+  const game = new RpslsGame(p1, p2);
+
+  it('should run the game', () => {
+    game.run();
+
+    const p1State = {
+      messages: [{ author: 's', message: 'Choose your weapon!' }],
+      name: 'p1',
+      result: '',
+      turn: '',
+      opponent: '',
+      online: true,
+    };
+
+    const p2State = {
+      messages: [{ author: 's', message: 'Choose your weapon!' }],
+      name: 'p2',
+      result: '',
+      turn: '',
+      opponent: '',
+      online: true,
+    };
+
+    expect(p1.state).toEqual(p1State);
+    expect(p2.state).toEqual(p2State);
+  });
+
+  it('should add a message from player two', () => {
+    const messages = [{ author: 's', message: 'Choose your weapon!' }, { author: 'p2', message: 'Hi there!' }];
+
+    p2.emitEvent('message', { author: 'p2', message: 'Hi there!' });
+
+    expect(p1.state.messages).toEqual(messages);
+    expect(p2.state.messages).toEqual(messages);
+  });
+
+  it('player one should choose a gesture', () => {
+    const gesture = 'rock';
+
+    p1.emitEvent('turn', 'rock');
+
+    expect(p1.state.turn).toEqual(gesture);
+  });
+
+  it('player two should choose a gesture', () => {
+    const gesture = 'paper';
+
+    p2.emitEvent('turn', 'paper');
+
+    expect(p2.state.turn).toEqual(gesture);
+  });
+
+  it('game should be finished', () => {
+    const p1State = {
+      messages: [
+        { author: 's', message: 'Choose your weapon!' },
+        { author: 'p2', message: 'Hi there!' },
+        { author: 's', message: 'You selected rock' },
+        { author: 's', message: 'You lost.' },
+      ],
+      name: 'p1',
+      result: 'lose',
+      turn: 'rock',
+      opponent: 'paper',
+      online: true,
+    };
+
+    const p2State = {
+      messages: [
+        { author: 's', message: 'Choose your weapon!' },
+        { author: 'p2', message: 'Hi there!' },
+        { author: 's', message: 'You selected paper' },
+        { author: 's', message: 'You won!' },
+      ],
+      name: 'p2',
+      result: 'win',
+      turn: 'paper',
+      opponent: 'rock',
+      online: true,
+    };
+
+    expect(p1.state).toEqual(p1State);
+    expect(p2.state).toEqual(p2State);
+  });
+
+  it('should add a message from player one', () => {
+    const p1Messages = [
+      { author: 's', message: 'Choose your weapon!' },
+      { author: 'p2', message: 'Hi there!' },
+      { author: 's', message: 'You selected rock' },
+      { author: 's', message: 'You lost.' },
+      { author: 'p1', message: 'Mb again?' },
+    ];
+
+    const p2Messages = [
+      { author: 's', message: 'Choose your weapon!' },
+      { author: 'p2', message: 'Hi there!' },
+      { author: 's', message: 'You selected paper' },
+      { author: 's', message: 'You won!' },
+      { author: 'p1', message: 'Mb again?' },
+    ];
+
+    p1.emitEvent('message', { author: 'p1', message: 'Mb again?' });
+
+    expect(p1.state.messages).toEqual(p1Messages);
+    expect(p2.state.messages).toEqual(p2Messages);
+  });
+
+  it('should reset the game', () => {
+    const p1State = {
+      messages: [
+        { author: 's', message: 'Choose your weapon!' },
+        { author: 'p2', message: 'Hi there!' },
+        { author: 's', message: 'You selected rock' },
+        { author: 's', message: 'You lost.' },
+        { author: 'p1', message: 'Mb again?' },
+        { author: 's', message: 'Next Round!' },
+      ],
+      name: 'p1',
+      result: '',
+      turn: '',
+      opponent: '',
+      online: true,
+    };
+
+    const p2State = {
+      messages: [
+        { author: 's', message: 'Choose your weapon!' },
+        { author: 'p2', message: 'Hi there!' },
+        { author: 's', message: 'You selected paper' },
+        { author: 's', message: 'You won!' },
+        { author: 'p1', message: 'Mb again?' },
+        { author: 's', message: 'Next Round!' },
+      ],
+      name: 'p2',
+      result: '',
+      turn: '',
+      opponent: '',
+      online: true,
+    };
+
+    p2.emitEvent('reset');
+    p1.emitEvent('reset');
+
+    expect(p1.state).toEqual(p1State);
+    expect(p2.state).toEqual(p2State);
+  });
+
+  it('should be a draw', () => {
+    const p1State = {
+      messages: [
+        { author: 's', message: 'Choose your weapon!' },
+        { author: 'p2', message: 'Hi there!' },
+        { author: 's', message: 'You selected rock' },
+        { author: 's', message: 'You lost.' },
+        { author: 'p1', message: 'Mb again?' },
+        { author: 's', message: 'Next Round!' },
+        { author: 's', message: 'You selected spock' },
+        { author: 's', message: 'Draw.' },
+      ],
+      name: 'p1',
+      result: 'draw',
+      turn: 'spock',
+      opponent: 'spock',
+      online: true,
+    };
+
+    const p2State = {
+      messages: [
+        { author: 's', message: 'Choose your weapon!' },
+        { author: 'p2', message: 'Hi there!' },
+        { author: 's', message: 'You selected paper' },
+        { author: 's', message: 'You won!' },
+        { author: 'p1', message: 'Mb again?' },
+        { author: 's', message: 'Next Round!' },
+        { author: 's', message: 'You selected spock' },
+        { author: 's', message: 'Draw.' },
+      ],
+      name: 'p2',
+      result: 'draw',
+      turn: 'spock',
+      opponent: 'spock',
+      online: true,
+    };
+
+    p1.emitEvent('turn', 'spock');
+    p2.emitEvent('turn', 'spock');
+
+    expect(p1.state).toEqual(p1State);
+    expect(p2.state).toEqual(p2State);
+  });
+
+  it('player two should leave', () => {
+    p2.emitEvent('disconnect');
+    expect(p1.state.online).toEqual(false);
+  });
+});
